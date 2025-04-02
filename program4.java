c14a357f20a795dfc73177f64a9f21caaa9f0618
Pallavirokde209 <pallavirokde209@gmail.com>
2025-04-02 21:12:48 +0530

    Add files via upload

---
+++ program4.java
@@ -0,0 +1,42 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(100);
+        System.out.println('A');
+        System.out.println("Pallavi");
+        System.out.println(true);
+           
+            System.out.println("================");
+            
+            String value1="hi buddies";
+            int value2 =10;
+            double value3= 345.00;
+            char value4= 'x';
+            boolean value5= false;
+            
+            System.out.println("++++++++++++++++++");
+            
+            int a=10;
+            int b=10;
+            String c="java";
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+            System.out.println(a%b);
+            System.out.println(a+c+b);
+            System.out.println(a+b+c);
+                
+               
+            System.out.println("======================");
+            
+            
+            
+        String container1="am learning java";
+        
+        System.out.println(container1);
+        System.out.println(container1);
+        System.out.println(container1);
+        System.out.println(container1);
+        System.out.println(container1);
+    }
+}
